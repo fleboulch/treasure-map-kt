1dc7cc7b8a649c432d5e263c71c9095855bac54c
Florent LE BOULCH <florent.leboulch@manomano.com>
2022-08-04 17:54:42 +0200

    First commit

---
+++ src/main/kotlin/com/flb/treasuremap/TreasuremapApplication.kt
@@ -0,0 +1,11 @@
+package com.flb.treasuremap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TreasuremapApplication
+
+fun main(args: Array<String>) {
+	runApplication<TreasuremapApplication>(*args)
+}
