58d51bd5e5136ebd0fb6afab09366d69ae493b53
Florent LE BOULCH <florent.leboulch@manomano.com>
2022-08-18 16:26:11 +0200

    fixup! An explorer should go ahead vertically and horizontally

--- src/main/kotlin/com/flb/treasuremap/domain/TreasureMap.kt
+++ src/main/kotlin/com/flb/treasuremap/domain/TreasureMap.kt
@@ -19,15 +19,15 @@ data class Position(val value: Pair<Int, Int>, val orientation: Orientation) {
 
     fun next(): Pair<Int, Int> {
         if (orientation == Orientation.EAST) {
-            return Pair(1, 0)
+            return Pair(value.first + 1, value.second)
         }
         if (orientation == Orientation.WEST) {
-            return Pair(0, 0)
+            return Pair(value.first - 1, value.second)
         }
         if (orientation == Orientation.SOUTH) {
-            return Pair(0, 1)
+            return Pair(value.first, value.second + 1)
         }
-        return Pair(1, 0)
+        return Pair(value.first, value.second -1)
     }
 
 
